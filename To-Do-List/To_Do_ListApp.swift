f50bf19843e666e97631283ac10c87bc090df9da
zoli0829 <zoli0829@gmail.com>
2025-09-29 17:21:39 +0100

    Initial commit

---
+++ To-Do-List/To_Do_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_Do_ListApp.swift
+//  To-Do-List
+//
+//  Created by Zoltan Vegh on 29/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct To_Do_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
